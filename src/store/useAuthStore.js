acf35d8fe63aa241f1cab4fcc8aaad02f43efa97
praff1351 <prafullihoo.7777@gmail.com>
2025-12-13 14:47:50 +0530

    Commit #11

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://chat-backend-3i49.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -90,6 +90,7 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
+      withCredentials: true,
       query: {
         userId: authUser._id,
       },
